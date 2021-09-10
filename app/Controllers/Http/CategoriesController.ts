26e14ed60d731dfb3b98c90cecc265d6561f21a8
Joao Ferreira <joaovlima07@hotmail.com>
2021-09-10 16:20:59 -0300

    feat: category

---
+++ app/Controllers/Http/CategoriesController.ts
@@ -0,0 +1,20 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CategoriesController {
+  public async index ({}: HttpContextContract) {
+  }
+
+
+  public async store ({}: HttpContextContract) {
+  }
+
+  public async show ({}: HttpContextContract) {
+  }
+
+
+  public async update ({}: HttpContextContract) {
+  }
+
+  public async destroy ({}: HttpContextContract) {
+  }
+}
