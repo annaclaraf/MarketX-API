8531acd9594e818471050058f37546f292c2ecff
Anna Clara <anisferraz@gmail.com>
2021-09-13 21:40:19 -0300

    feat: users

--- database/migrations/1631373607265_addresses.ts
+++ database/migrations/1631373607265_addresses.ts
@@ -7,6 +7,12 @@ export default class Addresses extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('users.id')
+        .onDelete('CASCADE')
+
       table.string('street').notNullable()
       table.string('city').notNullable()
       table.string('state').notNullable()
