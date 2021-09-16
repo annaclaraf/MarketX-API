b9dd37576ebbbf33ab2183de7572eed12a36a91c
Joao Ferreira <joaovlima07@hotmail.com>
2021-09-16 20:01:49 -0300

    fix: validator

--- app/Middleware/IsAdmin.ts
+++ app/Middleware/IsAdmin.ts
@@ -1,7 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class IsAdmin {
-  public async handle ({ auth, response }: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
 
     const { is_admin } = await auth.use('api').authenticate()
